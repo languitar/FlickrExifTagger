ebccf771b3e847e7c52bef709a2b0f78fe193a74
Johannes Wienke <languitar@semipol.de>
2014-11-18 23:54:01 +0000

    Factor out rules in something more declarative

--- flickrExifTagger.rb
+++ flickrExifTagger.rb
@@ -80,13 +80,55 @@ def doLogin
 
 end
 
+Matcher = Struct.new(:key, :regex)
+Rule = Struct.new(:matchers, :tags)
+
+$rules = [
+    # Nikon camera
+    Rule.new([Matcher.new('exif.Make', /Nikon/i), Matcher.new('exif.Lens', /60\.0 ?mm fi\/2\.8/)],
+             'lens:maker=Nikon,lens:aperture=2.8,lens:focallength=60,"lens:type=AF-S Micro Nikkor 60mm/2.8G ED"'),
+    Rule.new([Matcher.new('exif.Make', /Nikon/i), Matcher.new('exif.Lens', /10\.0-20\.0 ?mm f\/4\.0-5\.6/)],
+             'lens:maker=Sigma,lens:aperture=4.0-5.6,lens:focallength=10-20,"lens:type=10-20mm F4-5.6 EX DC HSM"'),
+    Rule.new([Matcher.new('exif.Make', /Nikon/i), Matcher.new('exif.Lens', /90\.0 ?mm f\/2\.8/)],
+             'lens:maker=Tamron,lens:aperture=2.8,lens:focallength=90,"lens:type=SP AF 90mm F/2.8 Di MACRO 1:1"'),
+    Rule.new([Matcher.new('exif.Make', /Nikon/i), Matcher.new('exif.Lens', /16(\.0)?-85(\.0)? ?mm f\/3\.5-5\.6/)],
+             'lens:maker=Nikon,lens:aperture=3.5-5.6,lens:focallength=16-85,"lens:type=AF-S DX Nikkor 16-85mm 1:3.5-5.6G ED VR"'),
+    Rule.new([Matcher.new('exif.Make', /Nikon/i), Matcher.new('exif.Lens', /70\.0-300\.0 ?mm f\/4\.0-5\.6/)],
+             'lens:maker=Tamron,lens:aperture=4.0-5.6,lens:focallength=70-300,"lens:type=SP AF 70-300 F/4-5.6 Di VC USD"'),
+    Rule.new([Matcher.new('exif.Make', /Nikon/i), Matcher.new('exif.Lens', /35(\.0)? ?mm f\/1\.8/)],
+             'lens:maker=Nikon,lens:aperture=1.8,lens:focallength=35,"lens:type=AF-S DX Nikkor 35mm 1:1.8G"'),
+    Rule.new([Matcher.new('exif.Make', /Nikon/i), Matcher.new('exif.Lens', /85(\.0)? ?mm f\/1.8/)],
+             'lens:maker=Nikon,lens:aperture=1.8,lens:focallength=85,"lens:type=AF Nikkor 85mm 1:1.8D"'),
+    Rule.new([Matcher.new('exif.Make', /Nikon/i), Matcher.new('exif.Lens', /50\.0 ?mm f\/1\.8/)],
+             'lens:maker=Nikon,lens:aperture=1.8,lens:focallength=50,"lens:type=AF Nikkor 50mm 1:1.8D"'),
+    Rule.new([Matcher.new('exif.Make', /Nikon/i), Matcher.new('exif.Lens', /55(\.0)?-200(\.0)? ?mm f\/4-5.6/)],
+             'lens:maker=Nikon,lens:aperture=4.0-5.6,lens:focallength=55-200,"lens:type=AF-S DX Zoom-Nikkor 55-200mm 1:4-5.6 G IF-ED VR"'),
+    # Olympus
+    Rule.new([Matcher.new('exif.Make', /Olympus/i), Matcher.new('exif.LensType', /14-42mm/)],
+             'lens:maker=Olympus,lens:aperture=3.5-5.6,lens:focallength=14-42,"lens:type=Zuiko Digital ED 14-42mm F3.5-5.6"'),
+    Rule.new([Matcher.new('exif.Lens', /Olympus/i), Matcher.new('exif.Lens', /14-42mm/)],
+             'lens:maker=Olympus,lens:aperture=3.5-5.6,lens:focallength=14-42,"lens:type=Zuiko Digital ED 14-42mm F3.5-5.6"'),
+    Rule.new([Matcher.new('exif.Make', /Olympus/i), Matcher.new('exif.LensType', /40-150mm/)],
+             'lens:maker=Olympus,lens:aperture=4.0-5.6,lens:focallength=40-150,"lens:type=Zuiko Digital ED 40-150mm F4.0-5.6"'),
+    Rule.new([Matcher.new('exif.Lens', /Olympus/i), Matcher.new('exif.Lens', /40-150mm/)],
+             'lens:maker=Olympus,lens:aperture=4.0-5.6,lens:focallength=40-150,"lens:type=Zuiko Digital ED 40-150mm F4.0-5.6"'),
+    Rule.new([Matcher.new('exif.Make', /Olympus/i), Matcher.new('exif.LensType', /1 22/)],
+             'lens:maker=Sigma,lens:aperture=2.8,lens:focallength=70-200,"lens:type=70-200mm F2.8 EX DG Makro HSM II"'),
+    Rule.new([Matcher.new('exif.Make', /Olympus/i), Matcher.new('exif.LensType', /35mm F3\.5 Macro/)],
+             'lens:maker=Olympus,lens:aperture=3.5,lens:focallength=35,"lens:type=Zuiko Digital 35mm F3.5 Macro"'),
+    Rule.new([Matcher.new('exif.Make', /Olympus/i), Matcher.new('exif.LensType', /Sigma 10-20mm/)],
+             'lens:maker=Sigma,lens:aperture=4.0-5.6,lens:focallength=10-20,"lens:type=10-20mm F4-5.6 EX DC HSM"'),
+    Rule.new([Matcher.new('exif.Make', /Olympus/i), Matcher.new('exif.LensType', /None/)],
+             'lens:maker=Lensbaby,lens:focallength=50,"lens:type=Lensbaby Muse Double Glass Optic"')
+]
+
 if __FILE__ == $0
 
   doLogin()
 
   # iterate through all photo pages
   stop = false
-  page = 1
+  page = 0
   numPhotos = 0
   until stop
     allUserPhotos = flickr.photos.search :user_id => $login.id, :page => page
@@ -98,53 +140,27 @@ if __FILE__ == $0
       puts("processing photo #{info.title}")
 
       exif = flickr.photos.getExif :photo_id => photo.id, :secret => photo.secret
-      tagsToAdd = nil
+
+      # create a has for all tags
+      mappedTags = {}
       exif.exif.each do |tag|
-        begin
-          if tag.tag == "LensType"
+        mappedTags["exif.#{tag.tag}"] = tag.raw
+      end
 
-            if tag.raw.include?("14-42mm")
-              tagsToAdd = 'lens:maker=Olympus,lens:aperture=3.5-5.6,lens:focallength=14-42,"lens:type=Zuiko Digital ED 14-42mm F3.5-5.6"'
-            elsif tag.raw.include?("40-150mm")
-              tagsToAdd = 'lens:maker=Olympus,lens:aperture=4.0-5.6,lens:focallength=40-150,"lens:type=Zuiko Digital ED 40-150mm F4.0-5.6"'
-            elsif tag.raw.include?("(1 22")
-              tagsToAdd = 'lens:maker=Sigma,lens:aperture=2.8,lens:focallength=70-200,"lens:type=70-200mm F2.8 EX DG Makro HSM II"'
-            elsif tag.raw.include?("35mm F3.5 Macro")
-              tagsToAdd = 'lens:maker=Olympus,lens:aperture=3.5,lens:focallength=35,"lens:type=Zuiko Digital 35mm F3.5 Macro"'
-            elsif tag.raw.include?("Sigma 10-20mm")
-              tagsToAdd = 'lens:maker=Sigma,lens:aperture=4.0-5.6,lens:focallength=10-20,"lens:type=10-20mm F4-5.6 EX DC HSM"'
-            elsif tag.raw.include?("None")
-              tagsToAdd = 'lens:maker=Lensbaby,lens:focallength=50,"lens:type=Lensbaby Muse Double Glass Optic"'
-            end
-            break
+      # compare tags to all known rules
+      tagsToAdd = nil
+      $rules.each do |rule|
 
-          elsif tag.tag == "Lens"
+        # find out whether this rules matches
+        matches = true
+        rule.matchers.each do |matcher|
+          matches &= matcher.regex.match mappedTags[matcher.key]
+        end
 
-            #puts("\tLens is: #{tag.raw}")
-
-            if tag.raw.include?("60.0 mm f/2.8")
-              tagsToAdd = 'lens:maker=Nikon,lens:aperture=2.8,lens:focallength=60,"lens:type=AF-S Micro Nikkor 60mm/2.8G ED"'
-            elsif tag.raw.include?("10.0-20.0 mm f/4.0-5.6")
-              tagsToAdd = 'lens:maker=Sigma,lens:aperture=4.0-5.6,lens:focallength=10-20,"lens:type=10-20mm F4-5.6 EX DC HSM"'
-            elsif tag.raw.include?("90.0 mm f/2.8")
-              tagsToAdd = 'lens:maker=Tamron,lens:aperture=2.8,lens:focallength=90,"lens:type=SP AF 90mm F/2.8 Di MACRO 1:1"'
-            elsif tag.raw.include?("16.0-85.0 mm f/3.5-5.6")
-              tagsToAdd = 'lens:maker=Nikon,lens:aperture=3.5-5.6,lens:focallength=16-85,"lens:type=AF-S DX Nikkor 16-85mm 1:3.5-5.6G ED VR"'
-            elsif tag.raw.include?("70.0-300.0 mm f/4.0-5.6")
-              tagsToAdd = 'lens:maker=Tamron,lens:aperture=4.0-5.6,lens:focallength=70-300,"lens:type=SP AF 70-300 F/4-5.6 Di VC USD"'
-            elsif tag.raw.include?("35.0 mm f/1.8")
-              tagsToAdd = 'lens:maker=Nikon,lens:aperture=1.8,lens:focallength=35,"lens:type=AF-S DX Nikkor 35mm 1:1.8G"'
-            elsif tag.raw.include?("85.0 mm f/1.8")
-              tagsToAdd = 'lens:maker=Nikon,lens:aperture=1.8,lens:focallength=85,"lens:type=AF Nikkor 85mm 1:1.8D"'
-            elsif tag.raw.include?("50.0 mm f/1.8")
-              tagsToAdd = 'lens:maker=Nikon,lens:aperture=1.8,lens:focallength=50,"lens:type=AF Nikkor 50mm 1:1.8D"'
-            elsif tag.raw.include?("55-200mm f/4-5.6")
-              tagsToAdd = 'lens:maker=Nikon,lens:aperture=4.0-5.6,lens:focallength=55-200,"lens:type=AF-S DX Zoom-Nikkor 55-200mm 1:4-5.6 G IF-ED VR"'
-            end
-            break
-
-          end
-
+        # if we have a matching rule, use the first one and ignore all others
+        if matches
+          tagsToAdd = rule.tags
+          break
         end
 
       end
@@ -153,11 +169,7 @@ if __FILE__ == $0
         flickr.photos.addTags :photo_id => photo.id, :tags => tagsToAdd
       else
         puts "  Unknown lens"
-        exif.exif.each do |tag|
-          if tag.tag.include?("Lens")
-            puts "    #{tag.tag} => #{tag.raw}"
-          end
-        end
+        puts mappedTags
       end
 
     end
